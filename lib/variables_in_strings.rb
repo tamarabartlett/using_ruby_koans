9ef2abdcab451ec07939b23d26a923d774e15567
Tamara Bartlett <tbartlett@tendrilinc.com>
2020-08-03 15:36:42 -0600

    Playing with variables in strings and mathing them

---
+++ lib/variables_in_strings.rb
@@ -0,0 +1,6 @@
+number_of_rigs = 2
+number_of_canopies_per_rig = 2
+
+puts "One rig has #{number_of_canopies_per_rig} canopies"
+puts "I have #{number_of_rigs} rigs"
+puts "Thus, I have #{number_of_rigs * number_of_canopies_per_rig} canopies"
